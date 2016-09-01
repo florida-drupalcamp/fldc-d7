00debc5157e2e0cc6833145822e34f8d37f08cc3
Mike Herchel <mike@herchel.com>
2016-09-01 08:15:39 -0400

    Forking FLDC14 into FLDC17.

---
+++ docroot/sites/all/themes/fldc17/js/fldc17.behaviors.js
@@ -0,0 +1,138 @@
+(function ($) {
+
+  Drupal.behaviors.fldc14BrowserClass = {
+    attach: function ( context, settings ) {
+      if ($.browser.msie && $.browser.version === 10) { // Detects IE10
+        $('html', context).addClass('ie10');
+      }
+      if (!!navigator.userAgent.match(/Trident\/7\./)) { // Detects IE11
+        $('html', context).addClass('ie11');
+      }
+    }
+  };
+
+  Drupal.behaviors.fldc14iesvg = {
+    attach: function ( context, settings ) {
+      // Detects IE9,10, & 11
+      if (($.browser.msie && $.browser.version >= 9) || (!!navigator.userAgent.match(/Trident\/7\./))) {
+        var img = document.getElementById('main-img');
+        var width = img.clientWidth;
+        var height = width * 1.29;  // the ratio of width to height of the drupal diver svg
+        $('#main-img', context).css('height', height);
+      }
+    }
+  };
+  
+  // Moving exploding animation to JS because nested animation doesn't work properly in FF
+  Drupal.behaviors.explodeShit = {
+    attach: function ( context, settings ) {
+      $('.diver-bubble > img', context).click(function() {
+        $(this).addClass('explode-this-shit')
+               .attr('src', '/sites/all/themes/fldc14/images/explosion.svg')
+               .animate({
+                 height: '300%',
+                 width: '300%'
+               }, 300, function () {
+                $(this).parent().css({ 'display' : 'none'});
+               });
+      });
+    }
+  };
+  
+  Drupal.behaviors.fldc14MobileMenu = {
+    attach: function ( context, settings ) {
+      $('#block-system-main-menu', context).after($('<div id="mobile-menu"></div>'));
+      $('#mobile-menu', context).once('mobile', function () {
+        $(this).click( function () {
+          $(this).toggleClass('header-menu-active');
+          $('#block-system-main-menu', context).toggleClass('header-menu-active');
+        });
+      });
+    }
+  };
+
+  Drupal.behaviors.sessionExpand = {
+    attach: function ( context, settings ) {
+      $('.session-expand', context).click(function() {
+        // $('.session', context).removeClass('js-expanded');
+        $(this).parent().toggleClass('js-expanded');
+      });
+    }
+  };
+  
+  Drupal.behaviors.headingHeightOnScroll = {
+    attach: function ( context, settings ) {
+      $('body', context).once('scroll', function () {
+        var $nav = $('#block-system-main-menu', context);
+        var $heading = $('.z-navigation', context);
+        $(window).scroll( function (){
+          var scrollTop = $(window, context).scrollTop();
+          if ( scrollTop >= 100 ) {
+            $heading.addClass('scroll-down');
+            $nav.addClass('scroll-down');
+          }
+          else if (scrollTop < 100) {
+            $heading.removeClass('scroll-down');
+            $nav.removeClass('scroll-down');
+          }
+        });
+      });
+    }
+  };
+ 
+  Drupal.behaviors.coolShitOnScroll = {
+    attach: function ( context, settings ) {
+      var $speaker = $('.speaker', context);
+      var $locationInfo = $('#location-info', context);
+      $(window).scroll( function (){
+        // Featured Speaker Pop
+        if($(window).scrollTop() + $(window).height() >= $speaker.offset().top) {
+          $speaker.addClass('scroll-down');
+        }
+        if($(window).scrollTop() + $(window).height() < $speaker.offset().top) {
+          $speaker.removeClass('scroll-down');
+        }
+        // Location Slide in
+        if($(window).scrollTop() + $(window).height() >= $locationInfo.offset().top) {
+          $locationInfo.addClass('scroll-down');
+        }
+        if($(window).scrollTop() + $(window).height() < $locationInfo.offset().top) {
+          $locationInfo.removeClass('scroll-down');
+        }
+      });
+    }
+  };
+
+  Drupal.behaviors.expandedMenuItems = {
+    attach: function ( context, settings ) {
+      $('#block-system-main-menu > .menu li.expanded', context).once('expanded-active', function () {
+        $(this).each( function () {
+          var windowWidth = $(window).width();
+          
+          if ( windowWidth <= 1025 ) {
+            $('> a', this).click( function ( e ) {
+              e.preventDefault();
+            });
+            
+            $(this).click( function () {
+              $(this).siblings('.expanded:not(.expanded-active-processed)').addClass('expanded-active-processed').children('.menu').slideToggle('fast');
+              $(this).toggleClass('expanded-active-processed');
+              $('.menu', this).slideToggle();
+            });
+          }
+        });
+      });
+    }
+  };
+
+  // Constructors
+  $.fn.areYouIE = function () {
+    if ($.browser.msie) {
+      $(this).addClass('ie-' + $.browser.version);
+    }
+    else if ($.browser.msie && $.browser.version <= 8) {
+      alert('Your browser is a bit outdated. For the best user experience, it is recommended that you upgrade from Internet Exporer ' + $.browser.version + ' to IE 10 or to another browser all together, like Firefox, Chrome, Safari or Opera. <a href="http://blogs.technet.com/b/security/archive/2013/08/15/the-risk-of-running-windows-xp-after-support-ends.aspx" target="_blank">Win XP Support ends April 2014</a>');
+    }
+  };
+
+})(jQuery);
---
+++ docroot/sites/all/themes/fldc17/templates/maintenance-page.tpl.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Florida DrupalCamp 2014</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+  <meta property="og:site_name" content="Florida DrupalCamp 2014" />
+  <meta property="og:image" content="/sites/all/themes/fldc14/coming_soon/drupaldiver-fallback.png" />
+  <meta name="description" content="Florida DrupalCamp will be held on March 8th, 2014 at Florida Technical College in Orlando, FL." />
+  <link rel="shortcut icon" href="/sites/all/themes/fldc14/coming_soon/drupal-diver-favicon.png" type="image/png" />
+  <link rel="stylesheet" href="/sites/all/themes/fldc14/coming_soon/fldc14.css" />
+  <!--[if lte IE 8]>
+    <link rel="stylesheet" type="text/css" href="ie8.css" />    
+  <![endif]-->   
+  <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+</head>
+<body>
+  <div id="water">
+    <div id="logo-container">
+      <img id="logo" src="/sites/all/themes/fldc14/coming_soon/drupal-diver-no-bubble.svg" alt="Florida Drupalcamp Logo" />
+      <img class="diver-bubble" id="bubble1" src="/sites/all/themes/fldc14/coming_soon/bubbles.svg" alt="bubbles">
+      <img class="diver-bubble" id="bubble2" src="/sites/all/themes/fldc14/coming_soon/bubbles.svg" alt="bubbles">
+      <img class="diver-bubble" id="bubble3" src="/sites/all/themes/fldc14/coming_soon/bubbles.svg" alt="bubbles">
+      <img class="diver-bubble" id="bubble4" src="/sites/all/themes/fldc14/coming_soon/bubbles.svg" alt="bubbles">
+      <img class="diver-bubble" id="bubble5" src="/sites/all/themes/fldc14/coming_soon/bubbles.svg" alt="bubbles">
+      <img id="diver-bubble-static" src="/sites/all/themes/fldc14/coming_soon/bubbles-static.png" alt="bubbles">
+    </div>
+  </div>
+  <h1 id="heading">March&nbsp; 8th,&nbsp; 2014</h1>
+  <h2 id="subheading">Orlando,&nbsp; FL</h2>
+  <form id="mc_embed_signup" action="http://fldrupalcamp.us4.list-manage1.com/subscribe/post?u=e7875ed8ccafb228670ba3056&amp;id=87e0827270" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+    <input type="email" value="" name="EMAIL" placeholder="Email Address" class="required email" id="mce-EMAIL">
+    <input type="submit" value="Stay Updated" name="subscribe" id="mc-embedded-subscribe">
+    <div id="mce-responses">
+      <div class="response" id="mce-error-response" style="display:none"></div>
+      <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>  
+  </form>
+  <section id="social">
+    <a target="_blank" title="Follow @FLDrupalcamp on Twitter" class="icon icon-twitter" href="http://twitter.com/fldrupalcamp"></a>
+    <a target="_blank" title="Like FLDrupalcamp on Facebook" class="icon icon-facebook" href="http://www.facebook.com/fldrupalcamp"></a>
+    <a target="_blank" title="+1 us on Google+" class="icon icon-google" href="https://plus.google.com/117346293893902416993"></a>
+    <a target="_blank" title="Florida Drupal Group" class="icon icon-drupal" href="http://groups.drupal.org/florida"></a>  
+  </section>
+  <footer id="footer">
+    <a title="Florida Drupalcamp 2011" href="http://2011.fldrupalcamp.org/">FLDC 2011</a>
+    <a title="Florida Drupalcamp 2012" href="http://2012.fldrupalcamp.org/">FLDC 2012</a>
+    <a title="Florida Drupalcamp 2013" href="http://2013.fldrupalcamp.org/">FLDC 2013</a>
+  </footer>
+</body>
+</html>
+ 
